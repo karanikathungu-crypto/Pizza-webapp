637e76c7c0a79f36e18b2707394671ca56e6fb44
karanikathungu-crypto <kathungudev@gmail.com>
2021-08-02 22:47:14 +0300

    Add variables for pizza specifications

--- Javs/main.js
+++ Javs/main.js
@@ -5,11 +5,11 @@ submitButton.addEventListener('submit',onsubmit);
 function onsubmit(e){
   e.preventDefault();
   alert("yahoo");
-let pizzaType=document.getElementById() ;
-let pizzaSize= ;
-let pizzaCrust= ;
-let pizzaTopping= ;
-let pizzaQuanity= ;
+let pizzaType=document.getElementsByClassName("form-check-input").value;
+let pizzaSize= document.getElementsByClassName("size").value;
+let pizzaCrust= document.getElementsByClassName("crust").value;
+let pizzaTopping= document.getElementsByClassName("topping").value;
+let pizzaQuanity= parseInt(document.getElementById("pizza-numbers").value);
 
 
 
