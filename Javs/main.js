71244e557d7e51d1cf7c42056f4e4d6d1d8416f3
karanikathungu-crypto <kathungudev@gmail.com>
2021-08-02 08:21:54 +0300

    Add function constructor for pizza

---
+++ Javs/main.js
@@ -0,0 +1,7 @@
+function Pizza(type,size,crust,topping,quantity){
+  this.pizzaType= type;
+  this.pizzaSize= size;
+  this.pizzaCrust= crust;
+  this.pizzaTopping= topping;
+  this.pizzaQuanity= quantity;
+}
