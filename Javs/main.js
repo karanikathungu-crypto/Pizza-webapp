f88980d3f955f24fa8bd558dd251abb6309f1a28
karanikathungu-crypto <kathungudev@gmail.com>
2021-08-02 22:23:36 +0300

    Add the event listener to button

--- Javs/main.js
+++ Javs/main.js
@@ -1,7 +1,24 @@
+
+// const pizzaUserForm=document.querySelector("#pizzaForm");
+const submitButton=document.getElementById("pizzaForm");
+submitButton.addEventListener('submit',onsubmit);
+function onsubmit(e){
+  e.preventDefault();
+  alert("yahoo");
+let pizzaType=document.getElementById() ;
+let pizzaSize= ;
+let pizzaCrust= ;
+let pizzaTopping= ;
+let pizzaQuanity= ;
+
+
+
 function Pizza(type,size,crust,topping,quantity){
   this.pizzaType= type;
   this.pizzaSize= size;
   this.pizzaCrust= crust;
   this.pizzaTopping= topping;
   this.pizzaQuanity= quantity;
+}
+var Pizza= new Pizza();
 }
