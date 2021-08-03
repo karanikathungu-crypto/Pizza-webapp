5dc208e6cb40ff3e6fbfcad700bf042a0915dcad
karanikathungu-crypto <kathungudev@gmail.com>
2021-08-03 09:27:03 +0300

    Add prices for toppings

--- Javs/main.js
+++ Javs/main.js
@@ -2,7 +2,6 @@ const submitButton=document.getElementById("pizzaForm");
 submitButton.addEventListener('submit',onsubmit);
 function onsubmit(e){
   e.preventDefault();
-  alert("yahoo");
 
 function Pizza(type,size,crust,topping,quantity){
   this.pizzaType= type;
@@ -21,8 +20,17 @@ let pizzaQuantity= parseInt(document.getElementById("pizza-numbers").value);
 var clientPizza= new Pizza(pizzaType,pizzaSize,pizzaCrust,pizzaTopping,pizzaQuantity);
 
 Pizza.prototype.displayPrice= function(){
-  if(Radio1.checked===true){
-    alert("The price of the pizza will be ksh 500");
+  if(Radio1.checked===true && Radio4.checked===true && Radio7.checked===true){
+    alert("The price of the crispy crust is ksh80 and tomato topping will be 100 ,the total price of the pizza will be ksh 500");
+  }
+  else if(Radio1.checked===true && Radio4.checked===true && Radio8.checked===true){
+    alert("The price of the crispy crust is ksh80 and onion topping will be 100 ,the total price of the pizza will be ksh 500");
+  }
+  else if(Radio1.checked===true && Radio4.checked===true && Radio9.checked===true){
+    alert("The price of the crispy crust is ksh80 and black olive topping will be 100 ,the total price of the pizza will be ksh 500");
+  }
+  else if(Radio1.checked===true){
+    alert("The price of the pizza will be ksh 500")
   }
   else if(Radio2.checked===true){
     alert("The price 0f the pizza will be ksh 700");
